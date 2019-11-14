405da318f6e52720040a1934c7d2156471d27118
Michael Wagstaff <michaelwagstaff2000@gmail.com>
2019-11-14 15:07:44 +0000

    First attempt at game of life code, not sure array swap works properly

--- src/gol.go
+++ src/gol.go
@@ -6,13 +6,25 @@ import (
 	"strings"
 )
 
+func customMod(index int, max int) int {
+	if index >= max {
+		return index - max
+	} else if index < 0 {
+		return index + max
+	} else {
+		return index
+	}
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p golParams, d distributorChans, alive chan []cell) {
 
 	// Create the 2D slice to store the world.
 	world := make([][]byte, p.imageHeight)
+	newWorld := make([][]byte, p.imageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.imageWidth)
+		newWorld[i] = make([]byte, p.imageWidth)
 	}
 
 	// Request the io goroutine to read in the image with the given filename.
@@ -34,10 +46,29 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 	for turns := 0; turns < p.turns; turns++ {
 		for y := 0; y < p.imageHeight; y++ {
 			for x := 0; x < p.imageWidth; x++ {
-				// Placeholder for the actual Game of Life logic: flips alive cells to dead and dead cells to alive.
-				world[y][x] = world[y][x] ^ 0xFF
+				var neighboursAlive = 0
+				for i := y - 1; i <= y + 1; i++ {
+					for j := x - 1; j <= x + 1; j++ {
+						if world[customMod(i, p.imageHeight)][customMod(i, p.imageWidth)] == 0xFF {
+							neighboursAlive++
+						}
+					}
+				}
+				if world[y][x] == 0xFF {
+					neighboursAlive--
+					if neighboursAlive == 2 || neighboursAlive == 3 {
+						newWorld[y][x] = 0xFF
+					} else {
+						newWorld[y][x] = 0x00
+					}
+				} else if neighboursAlive == 3 {
+					newWorld[y][x] = 0xFF
+				}
 			}
 		}
+		temp := newWorld
+		newWorld = world
+		world = temp
 	}
 
 	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
@@ -45,7 +76,7 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 	// Go through the world and append the cells that are still alive.
 	for y := 0; y < p.imageHeight; y++ {
 		for x := 0; x < p.imageWidth; x++ {
-			if world[y][x] != 0 {
+			if newWorld[y][x] != 0 {
 				finalAlive = append(finalAlive, cell{x: x, y: y})
 			}
 		}
