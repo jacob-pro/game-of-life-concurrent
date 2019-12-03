867c6ce3450beac7a9f00b841af0541f78c0f52a
jacob-pro <jacob@jhalsey.com>
2019-12-03 22:57:31 +0000

    Benchmarks2 (#13)
    * add script
    
    * re-enable all
    
    * delete folder
    
    * make them a bit quicker
    
    * add +x

---
+++ src/benchmark.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+go test -c -o gameoflife.test
+rm -r benchmarks
+mkdir benchmarks
+
+benchtime=5x
+benchmark=512x512x8
+
+for impl in serial parallel halo parallelshared rust
+do
+    echo implementation ${impl} on our solution
+    ./gameoflife.test -test.run XXX -test.bench /${benchmark} -test.benchtime ${benchtime} -i ${impl} > benchmarks/${impl}.txt
+done
+
+echo baseline solution
+./baseline.test -test.run XXX -test.bench /${benchmark} -test.benchtime ${benchtime} > benchmarks/baseline.txt
