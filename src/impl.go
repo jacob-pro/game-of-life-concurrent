825c2608cd840bae7438029c8b38ca54ed104332
Jacob Halsey <jacob@jhalsey.com>
2019-12-03 14:26:48 +0000

    make rust the default again

--- src/impl.go
+++ src/impl.go
@@ -24,7 +24,7 @@ const (
 	implementationRust
 )
 
-const implementationDefault = implementationHalo
+const implementationDefault = implementationRust
 
 // This is case insensitive
 func implementationFromName(s string) (implementationEnum, error) {
